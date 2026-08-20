c640819344c42c8927ac2460e7b231e4f5b7c385
Mahmoud Shoura <1m.shoura1@gmail.com>
2026-08-20 17:39:51 +0300

    adding DbContext class and configure EF Core and SQL Server persistence

--- PersonalToDo_Freelance/Data/ApplicationDbContext.cs
+++ PersonalToDo_Freelance/Data/ApplicationDbContext.cs
@@ -16,5 +16,61 @@ namespace PersonalToDo_Freelance.Data
         public DbSet<TodoTask> Tasks { get; set; } = null!;
         public DbSet<RecurrenceRule> RecurrenceRules { get; set; } = null!;
         public DbSet<TaskOccurrence> TaskOccurrences { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>(b =>
+            {
+                b.ToTable("Categories");
+                b.HasKey(x => x.Id);
+                b.Property(x => x.Name).IsRequired().HasMaxLength(200);
+                b.Property(x => x.Description).HasMaxLength(4000);
+                b.Property(x => x.UserId).IsRequired().HasMaxLength(450);
+                b.HasIndex(x => x.UserId);
+                b.HasMany(x => x.Tasks).WithOne(t => t.Category).HasForeignKey(t => t.CategoryId).OnDelete(DeleteBehavior.Restrict);
+            });
+
+            modelBuilder.Entity<TodoTask>(b =>
+            {
+                b.ToTable("Tasks");
+                b.HasKey(x => x.Id);
+                b.Property(x => x.Title).IsRequired().HasMaxLength(200);
+                b.Property(x => x.Description).HasMaxLength(4000);
+                b.Property(x => x.UserId).IsRequired().HasMaxLength(450);
+                b.Property(x => x.Priority).IsRequired();
+                b.Property(x => x.Status).IsRequired();
+                b.Property(x => x.IsDeleted).IsRequired();
+                b.HasIndex(x => x.UserId);
+                b.HasIndex(x => new { x.UserId, x.DueDate });
+                b.HasIndex(x => new { x.UserId, x.Status });
+                b.HasIndex(x => new { x.UserId, x.CategoryId });
+                b.HasOne(x => x.RecurrenceRule).WithOne(r => r.TodoTask).HasForeignKey<RecurrenceRule>(r => r.TodoTaskId).OnDelete(DeleteBehavior.Cascade);
+                b.HasMany(x => x.Occurrences).WithOne(o => o.TodoTask).HasForeignKey(o => o.TodoTaskId).OnDelete(DeleteBehavior.Restrict);
+            });
+
+            modelBuilder.Entity<RecurrenceRule>(b =>
+            {
+                b.ToTable("RecurrenceRules");
+                b.HasKey(x => x.Id);
+                b.Property(x => x.UserId).IsRequired().HasMaxLength(450);
+                b.Property(x => x.Type).IsRequired();
+                b.Property(x => x.Interval).IsRequired();
+                b.HasIndex(x => x.UserId);
+                b.HasIndex(x => x.TodoTaskId).IsUnique();
+            });
+
+            modelBuilder.Entity<TaskOccurrence>(b =>
+            {
+                b.ToTable("TaskOccurrences");
+                b.HasKey(x => x.Id);
+                b.Property(x => x.OccurrenceDate).IsRequired();
+                b.Property(x => x.Status).IsRequired();
+                b.Property(x => x.IsDeleted).IsRequired();
+                b.HasIndex(x => new { x.TodoTaskId, x.OccurrenceDate });
+                b.HasOne(x => x.RecurrenceRule).WithMany().HasForeignKey(x => x.RecurrenceRuleId).OnDelete(DeleteBehavior.SetNull);
+            });
+        }
     }
 }
